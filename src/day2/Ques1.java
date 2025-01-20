7597cf46e1c1aaf6d96976300819a575c16d2b62
Baibhavi <baibhavipandey@gmail.com>
2025-01-20 14:18:34 +0530

    Added: Day 2 Assignment in feature branch

---
+++ src/day2/Ques1.java
@@ -0,0 +1,7 @@
+package day2;
+
+public class Ques1{
+	public static void main(String args[]){
+			System.out.println("Welcome to Bridgelabz!");
+	}
+}
---
+++ src/day2/Ques3.java
@@ -0,0 +1,13 @@
+package day2;
+
+import java.util.Scanner;
+public class Ques3{
+	public static void main(String args[]){
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the temperature in Celsius: ");
+			int celsius = sc.nextInt();
+			int fahren = (celsius * 9/5) + 32;
+			System.out.println("Temperature in Fahrenheit: " + fahren);
+			sc.close();
+	}
+}
